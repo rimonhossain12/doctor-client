e82db817f1eb97355c65438cb4ff453382e332a6
rimonspi12 <rimonmpi@gmail.com>
2022-11-06 16:14:40 +0600

    infos images center

--- src/Pages/Home/Information.js
+++ src/Pages/Home/Information.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import Infos from './Infos';
+import clock from '../../../src/assets/icons/clock.svg';
+import marker from '../../../src/assets/icons/marker.svg';
+import phone from '../../../src/assets/icons/phone.svg';
 
 const Information = () => {
     return (
-        <div>
-            <h2>This is Information</h2>
+        <div className='grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 mb-5'>
+            <Infos name="opening Hour" img={clock} desc="Lorem ipsum dolor sit amet consectetur 
+            adipisicing elit. Totam, repellendus." bgColor="bg-gradient-to-r from-primary to-secondary text-white"></Infos>
+            <Infos name="visit our location" img={marker} desc="Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam, 
+            repellendus." bgColor="bg-[#3A4256] text-white">
+            </Infos>
+            <Infos name="contact us now" img={phone} desc="Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+            Totam, repellendus." bgColor="bg-gradient-to-r from-primary to-secondary text-white"></Infos>
         </div>
     );
 };
--- src/Pages/Home/Infos.js
+++ src/Pages/Home/Infos.js
@@ -1,9 +1,15 @@
 import React from 'react';
 
-const Infos = () => {
+const Infos = ({ name, desc, bgColor, img }) => {
     return (
-        <div>
-            <h2>This is Infos</h2>
+        <div className={`card lg:card-side shadow-xl ${bgColor} lg:px-3`}>
+            {/* <div className='mt-5'> */}
+            <figure className='sm: mt-5'><img src={img} alt="Album" /></figure>
+            {/* </div> */}
+            <div className="card-body text-start">
+                <h2 className="card-title">{name}</h2>
+                <p>{desc}.</p>
+            </div>
         </div>
     );
 };
