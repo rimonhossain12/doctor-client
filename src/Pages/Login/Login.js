14c2967959616ca30dfc1e063de3d597f302c683
rimonspi12 <rimonmpi@gmail.com>
2022-11-07 22:09:05 +0600

    register page added

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,10 +1,44 @@
 import React from 'react';
 import { useForm } from "react-hook-form";
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import { useSignInWithEmailAndPassword } from 'react-firebase-hooks/auth';
+import auth from '../../firebase.init';
+import LoadingSpinner from '../Shared/LoadingSpinner';
+
+
 
 const Login = () => {
-	const { register, handleSubmit, watch, formState: { errors } } = useForm();
-	const onSubmit = data => console.log(data);
+	const { register, formState: { errors }, handleSubmit } = useForm();
+
+	const [
+		signInWithEmailAndPassword,
+		user,
+		loading,
+		error,
+	] = useSignInWithEmailAndPassword(auth);
+
+	const onSubmit = (e, data) => {
+		e.preventDefault();
+		signInWithEmailAndPassword(data.email, data.password);
+		console.log(data);
+	};
+
+
+	let signInError;
+	let navigate = useNavigate();
+
+	if (error) {
+		signInError = <p className='text-red-500 text-thin'>{error.message}</p>
+	}
+
+	if (loading) {
+		return <LoadingSpinner />
+	}
+
+	if (user) {
+		navigate('/home')
+	}
+
 
 
 	return (
@@ -12,67 +46,61 @@ const Login = () => {
 			<div className="card w-96 bg-base-100 shadow-xl">
 				<div className="card-body">
 					<h2 className="text-center font-semibold text-2xl text-accent">Login</h2>
-					<div className="form-control w-full max-w-xs">
 
-						<form onSubmit={handleSubmit(onSubmit)}>
+					<form onSubmit={handleSubmit(onSubmit)}>
+						<div className="form-control w-full max-w-xs">
 							<label className="label">
 								<span className="label-text">Email</span>
 							</label>
-
-							<input
+							<input type="email"
+								placeholder="Your Email"
+								className="input input-bordered w-full max-w-xs"
 								{...register("email",
 									{
 										required: {
 											value: true,
-											message: 'Email is required'
+											message: 'Email is required!'
 										},
 										pattern: {
 											value: /[a-z0-9]+@[a-z]+\.[a-z]{2,3}/,
 											message: 'Provide a valid Email'
 										}
 									}
-
 								)}
 								aria-invalid={errors.email ? "true" : "false"}
-								type="text" placeholder="Type here"
-								className="input input-bordered w-full max-w-xs"
 							/>
-
 							<label className="label">
-								{errors.email?.type === 'required' && <p className='text-red-500 text-start font-thin'>{errors?.email.message}</p>}
-								{errors.email?.type === 'pattern' && <p className='text-red-500 text-start font-thin'>{errors?.email.message}</p>}
+								{errors.email?.type === 'required' && <p className='text-red-500 text-thin' role="alert">{errors.email.message}</p>}
+								{errors.email?.type === 'pattern' && <p className='text-red-500 text-thin' role="alert">{errors.email.message}</p>}
 							</label>
+						</div>
+
+
+						<div className="form-control w-full max-w-xs">
 							<label className="label">
 								<span className="label-text">Password</span>
 							</label>
-
-							<input
+							<input type="password"
+								placeholder="Type here"
+								className="input input-bordered w-full max-w-xs"
 								{...register("password",
 									{
-										required: {
-											value: true,
-											message: 'Password is required'
-										},
 										minLength: {
 											value: 6,
-											message: 'Must be provide 6 character or longer'
+											message: 'Must be 6 character or Longer!'
 										}
 									}
-
 								)}
-								aria-invalid={errors.email ? "true" : "false"}
-								type="text" placeholder="Type here"
-								className="input input-bordered w-full max-w-xs"
+								aria-invalid={errors.password ? "true" : "false"}
 							/>
-
 							<label className="label">
-								{errors.password?.type === 'required' && <p className='text-red-500 text-start font-thin'>{errors?.password.message}</p>}
-								{errors.password?.type === 'minLength' && <p className='text-red-500 text-start font-thin'>{errors?.password.message}</p>}
+								{errors.password?.type === 'required' && <p className='text-red-500 text-thin' role="alert">{errors.password.message}</p>}
+								{errors.password?.type === 'minLength' && <p className='text-red-500 text-thin' role="alert">{errors.password.message}</p>}
 							</label>
-
-							<button className='btn w-full btn-accent text-white'>Login</button>
-						</form>
-					</div>
+						</div>
+						{signInError}
+						<input type="Submit" className='btn btn-outline w-full max-w-xs' value='Login' />
+					</form>
 
 					<p className='py-2'>New to Doctor Portal?
 						<Link to="/signup"><span className='text-secondary'> Create New Account</span></Link>
--- src/Pages/Login/SignUp.js
+++ src/Pages/Login/SignUp.js
@@ -2,8 +2,12 @@ import React from 'react';
 
 const SignUp = () => {
     return (
-        <div>
-            <h2>This is Sign up folder</h2>
+        <div className='flex justify-center items-center h-screen'>
+            <div className="card w-96 bg-base-100 shadow-xl">
+                <div className="card-body">
+                    <h2 className="text-center text-secondary text-2xl font-semibold">SignUp</h2>
+                </div>
+            </div>
         </div>
     );
 };
